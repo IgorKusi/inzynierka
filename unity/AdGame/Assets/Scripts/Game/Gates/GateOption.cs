d6079b3075f0b95567f6e8a957556fd255b3755c
IgorKusidel <igor.kusidel@gmail.com>
2026-06-24 22:02:40 +0200

    Replace glass materials & update prefabs

--- unity/AdGame/Assets/Scripts/Game/Gates/GateOption.cs
+++ unity/AdGame/Assets/Scripts/Game/Gates/GateOption.cs
@@ -1,7 +1,16 @@
+using TMPro;
 using UnityEngine;
 
 public class GateOption : MonoBehaviour
 {
+    [Header("Text Materials")]
+    [SerializeField] private Material AddOptionMaterial;
+    [SerializeField] private Material SubOptionMaterial;
+    
+    
+    [SerializeField]
+    private ParticleSystem optionEffect;
+    
     [SerializeField]
     private bool isLeftOption;
 
@@ -11,30 +20,51 @@ public class GateOption : MonoBehaviour
 
     private void Start()
     {
-        gate =
-            GetComponentInParent<Gate>();
+        gate = GetComponentInParent<Gate>();
+        GateOperationType operation;
+        if (isLeftOption)
+        {
+            operation = gate.leftOperation;
+        }
+        else
+        {
+            operation = gate.rightOperation;
+        }
+
+        if (operation == GateOperationType.Add || operation == GateOperationType.Multiply)
+        {
+            GetComponent<Renderer>().material = AddOptionMaterial;
+        }
+        else if (operation == GateOperationType.Subtract || operation == GateOperationType.Divide)
+        {
+            GetComponent<Renderer>().material = SubOptionMaterial;
+        }
+        else
+        {
+            throw new System.Exception("Unknown operation type");
+        }
     }
 
-    private void OnTriggerEnter(
-        Collider other
-    )
+    private void OnTriggerEnter(Collider other)
     {
         if (activated)
         {
             return;
         }
 
-        if (
-            !other.CompareTag("Player")
-        )
+        if (!other.CompareTag("Player"))
         {
             return;
         }
 
         activated = true;
 
-        gate.ApplyOption(
-            isLeftOption
-        );
+        gate.ApplyOption(isLeftOption);
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        optionEffect.Play();
     }
 }
